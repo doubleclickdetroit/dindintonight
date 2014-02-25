056e35a092df931e1bcedaddc0ee2e3399271a16
Rob Garrison <rgarrison3@gmail.com>
2014-02-24 22:25:41 -0500

    Updated URLs

--- core/urls.py
+++ core/urls.py
@@ -3,46 +3,17 @@ from django.views.generic import TemplateView
 
 import views
 
-# import anonymous_users.urls as anonymous_users_urls
-# import cards.urls           as cards_urls
-# import clients.urls         as clients_urls
-# import locations.urls       as locations_urls
-# import meals.urls           as meals_urls
-# import users.urls           as users_urls
-# import vendors.urls         as vendors_urls
-
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # # Home
-    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     # # url(r'^$', TemplateView.as_view(template_name='base.html')),
 
     # All Auth
     url(r'^accounts/', include('allauth.urls')),
-
-    # # Anonymous Users Module URLs
-    # url(r'^', include(anonymous_users_urls)),
-
-    # # Cards Module URLs
-    # url(r'^', include(cards_urls)),
-
-    # # Clients Module URLs
-    # url(r'^', include(clients_urls)),
-
-    # Locations Module URLs
-    # url(r'^', include(locations_urls)),
-
-    # # Meals Module URLs
-    # url(r'^', include(meals_urls)),
-
-    # # Users Module URLs
-    # url(r'^', include(user_urls)),
-
-    # Vendors Module URLs
-    # url(r'^', include(vendors_urls)),
 
     # Browseable Rest Framework Urls
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
